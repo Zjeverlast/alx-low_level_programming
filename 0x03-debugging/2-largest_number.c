5dc8883a8cbb30ec4581d19b251ea887c369656d
Zjeverlast <joemwangijr254@gmail.com>
2023-02-24 06:29:17 -0800

    zero_is_the_largest_no

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -3,7 +3,7 @@
 /**
  * largest_number - returns the largest of 3 numbers
  * @a: first integer
- * @b:second integer
+ * @b: second integer
  * @c: third integer
  * Return: largest number
  */
@@ -18,7 +18,7 @@ int largest_number(int a, int b, int c)
 	}
 	else if (b > a && a > c)
 	{
-		largest = b;
+	largest = b;
 	}
 	else
 	{
